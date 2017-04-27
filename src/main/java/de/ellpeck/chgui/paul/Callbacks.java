5777595c4950b3e5c459fde71c2dadce631123a3
Aaron Mills <aaron_m@creeperhost.net>
2017-04-27 22:29:55 +0100

    Paul's location stuff

--- src/main/java/de/ellpeck/chgui/paul/Callbacks.java
+++ src/main/java/de/ellpeck/chgui/paul/Callbacks.java
@@ -1,16 +1,40 @@
 package de.ellpeck.chgui.paul;
 
+import java.io.InputStream;
+import java.lang.reflect.Type;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
-public final class Callbacks{
+import org.apache.commons.compress.utils.IOUtils;
 
+import com.google.common.reflect.TypeToken;
+import com.google.gson.*;
+
+public final class Callbacks {
+    public static Map<Integer, String> locations = new HashMap<Integer, String>();
     public static Map<Integer, String> getAllServerLocations(){
-        Map<Integer, String> map = new HashMap<Integer, String>();
-        map.put(0, "Test City");
-        map.put(1, "Tester City");
-        map.put(2, "Testest City");
-        return map;
+        Map<String, Integer> rawMap = new HashMap<String,Integer>();
+        InputStream in;
+        try {
+            in = new URL( "https://www.creeperhost.net/json/locations" ).openStream();
+            String jsonData = "";
+            byte[] tmp = IOUtils.toByteArray(in);
+            jsonData = new String(tmp);
+            IOUtils.closeQuietly(in);
+            Gson g = new Gson();
+            Type type = new TypeToken<Map<String, Integer>>(){}.getType();
+            rawMap = g.fromJson(jsonData, type);
+        } catch(Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+        for(Map.Entry<String, Integer> entry : rawMap.entrySet()) {
+            String key = entry.getKey();
+            Integer value = entry.getValue();
+            locations.put(value, Character.toUpperCase(key.charAt(0)) + key.substring(1));
+        }
+        return locations;
     }
 
     //Not called yet, but you should process the order here
