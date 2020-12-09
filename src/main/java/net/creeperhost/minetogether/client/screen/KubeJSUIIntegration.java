67eb1891dfabe981d6fcb7937270ae584c43192d
LatvianModder <latvianmodder@gmail.com>
2020-12-09 17:56:21 +0200

    Added KubeJS UI Integration

---
+++ src/main/java/net/creeperhost/minetogether/client/screen/KubeJSUIIntegration.java
@@ -0,0 +1,18 @@
+package net.creeperhost.minetogether.client.screen;
+
+import net.creeperhost.minetogether.api.Order;
+import net.creeperhost.minetogether.client.screen.chat.MTChatScreen;
+import net.creeperhost.minetogether.client.screen.order.GuiGetServer;
+import net.creeperhost.minetogether.client.screen.serverlist.gui.FriendsListScreen;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screen.Screen;
+
+/**
+ * @author LatvianModder
+ */
+public interface KubeJSUIIntegration
+{
+	Consumer<Screen> CHAT = screen -> Minecraft.getInstance().displayGuiScreen(new MTChatScreen(screen));
+	Consumer<Screen> FRIENDS_LIST = screen -> Minecraft.getInstance().displayGuiScreen(new FriendsListScreen(screen));
+	Consumer<Screen> ORDER = screen -> Minecraft.getInstance().displayGuiScreen(GuiGetServer.getByStep(0, new Order()));
+}
