f00ea2c253d484d1a28b8d99390de68d3f330b10
Travis_N <travis_n@creeperhost.net>
2021-05-21 17:02:49 +0100

    Bring back killing of the watchdog thread

--- common/src/main/java/net/creeperhost/minetogether/MineTogetherServer.java
+++ common/src/main/java/net/creeperhost/minetogether/MineTogetherServer.java
@@ -4,6 +4,31 @@ public class MineTogetherServer
 {
     public static void init()
     {
+        killWatchDog();
+    }
 
+    public static void killWatchDog()
+    {
+        Thread watchdogThread = getThreadByName("Server Watchdog");
+        if(watchdogThread == null)
+        {
+            MineTogether.logger.info("Watchdog thread not found");
+            return;
+        }
+
+        try
+        {
+            MineTogether.logger.info("We're about to kill the Server Watchdog. Don't worry, we'll resuscitate it! The next error is normal.");
+            watchdogThread.interrupt();
+        } catch (Exception ignored) {}
+    }
+
+    public static Thread getThreadByName(String threadName)
+    {
+        for (Thread thread : Thread.getAllStackTraces().keySet())
+        {
+            if (thread.getName().equals(threadName)) return thread;
+        }
+        return null;
     }
 }
