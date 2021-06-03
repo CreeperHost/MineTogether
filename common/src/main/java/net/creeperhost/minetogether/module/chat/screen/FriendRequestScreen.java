e1e6dc85f0dbccd7db07b0fdef7927c77c3ae7c6
Travis_N <travis_n@creeperhost.net>
2021-06-03 12:51:00 +0100

    Cleanup FriendRequestScreen

--- common/src/main/java/net/creeperhost/minetogether/module/chat/screen/FriendRequestScreen.java
+++ common/src/main/java/net/creeperhost/minetogether/module/chat/screen/FriendRequestScreen.java
@@ -4,6 +4,7 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import net.creeperhost.minetogether.MineTogetherClient;
 import net.creeperhost.minetogetherlib.chat.ChatCallbacks;
 import net.creeperhost.minetogetherlib.chat.ChatHandler;
+import net.creeperhost.minetogetherlib.chat.MineTogetherChat;
 import net.creeperhost.minetogetherlib.chat.data.Profile;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.components.Button;
@@ -11,6 +12,8 @@ import net.minecraft.client.gui.components.EditBox;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.resources.language.I18n;
 import net.minecraft.network.chat.TranslatableComponent;
+
+import java.util.concurrent.CompletableFuture;
 
 public class FriendRequestScreen extends Screen
 {
@@ -46,28 +49,28 @@ public class FriendRequestScreen extends Screen
         buttons.clear();
         mc.keyboardHandler.setSendRepeatsToGui(true);
         
-        this.addButton(cancelBtn = new Button(width / 2 - 180, height - 50, 80, 20, new TranslatableComponent("Cancel"), (button) ->
-        {
-            Minecraft.getInstance().setScreen(parent);
-        }));
+        this.addButton(cancelBtn = new Button(width / 2 - 180, height - 50, 80, 20, new TranslatableComponent("Cancel"), (button) -> Minecraft.getInstance().setScreen(parent)));
         
         this.addButton(acceptBtn = new Button(width / 2 + 100, height - 50, 80, 20, accept ? new TranslatableComponent("Accept") : new TranslatableComponent("Send request"), (buttons) ->
         {
             if (accept)
             {
                 ChatHandler.acceptFriendRequest(chatInternalName, friendName);
-                new Thread(() -> ChatCallbacks.addFriend(friendCode, nameEntry.getValue().trim(), MineTogetherClient.getUUID())).start();
+                CompletableFuture.runAsync(() -> ChatCallbacks.addFriend(friendCode, nameEntry.getValue().trim(), MineTogetherClient.getUUID()), MineTogetherChat.otherExecutor);
             } else
             {
                 ChatHandler.sendFriendRequest(chatInternalName, nameEntry.getValue().trim());
             }
             Minecraft.getInstance().setScreen(parent);
         }));
-        
-        nameEntry = new EditBox(mc.font, width / 2 - 100, height / 2 - 10, 200, 20, new TranslatableComponent(""));
+
+        addButton(nameEntry = new EditBox(mc.font, width / 2 - 100, height / 2 - 10, 200, 20, new TranslatableComponent("")));
+
         if (first)
+        {
             nameEntry.setValue(profile.getUserDisplay()); // default to player name
-        first = false;
+            first = false;
+        }
         
         acceptBtn.active = nameEntry.getValue().trim().length() >= 3;
         nameEntry.setFocus(true);
@@ -79,31 +82,8 @@ public class FriendRequestScreen extends Screen
     {
         renderDirtBackground(1);
         super.render(matrixStack, mouseX, mouseY, partialTicks);
-//        nameEntry.setEnableBackgroundDrawing(true);
         nameEntry.render(matrixStack, mouseX, mouseY, partialTicks);
         drawCenteredString(matrixStack, mc.font, accept ? I18n.get("minetogether.friend.acceptgui") : I18n.get("minetogether.friend.addgui"), width / 2, 5, 0xFFFFFFFF);
         drawCenteredString(matrixStack, mc.font, accept ? I18n.get("minetogether.friend.acceptname") : I18n.get("minetogether.friend.addname"), width / 2, height / 2 - 30, 0xFFFFFFFF);
-    }
-    
-    @Override
-    public boolean mouseClicked(double mouseX, double mouseY, int mouseButton)
-    {
-        nameEntry.mouseClicked(mouseX, mouseY, mouseButton);
-        return super.mouseClicked(mouseX, mouseY, mouseButton);
-    }
-    
-    @Override
-    public boolean keyPressed(int p_keyPressed_1_, int p_keyPressed_2_, int p_keyPressed_3_)
-    {
-        nameEntry.keyPressed(p_keyPressed_1_, p_keyPressed_2_, p_keyPressed_3_);
-        acceptBtn.active = nameEntry.getValue().trim().length() >= 3;
-        return super.keyPressed(p_keyPressed_1_, p_keyPressed_2_, p_keyPressed_3_);
-    }
-    
-    @Override
-    public boolean charTyped(char p_charTyped_1_, int p_charTyped_2_)
-    {
-        nameEntry.charTyped(p_charTyped_1_, p_charTyped_2_);
-        return false;
     }
 }
