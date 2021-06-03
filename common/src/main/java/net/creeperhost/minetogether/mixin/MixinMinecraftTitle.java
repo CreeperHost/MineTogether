b492fa06da705dd2987c03136bd65a2cc97927f7
Travis_N <travis_n@creeperhost.net>
2021-06-03 12:53:11 +0100

    Format Aarons code

--- common/src/main/java/net/creeperhost/minetogether/mixin/MixinMinecraftTitle.java
+++ common/src/main/java/net/creeperhost/minetogether/mixin/MixinMinecraftTitle.java
@@ -12,12 +12,15 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Minecraft.class)
-public class MixinMinecraftTitle {
+public class MixinMinecraftTitle
+{
     @Shadow @Nullable private ServerData currentServer;
 
     @Inject(at = @At("RETURN"), method = "createTitle()Ljava/lang/String;", cancellable = true)
-    private void createTitle(CallbackInfoReturnable<String> cir) {
-        if (ConnectHelper.isShared() || (currentServer != null && currentServer.ip.contains("minetogether.ch.tools") && currentServer.ip.startsWith("connect"))) {
+    private void createTitle(CallbackInfoReturnable<String> cir)
+    {
+        if (ConnectHelper.isShared() || (currentServer != null && currentServer.ip.contains("minetogether.ch.tools") && currentServer.ip.startsWith("connect")))
+        {
             cir.setReturnValue(cir.getReturnValue().replace(I18n.get("title.multiplayer.lan"), I18n.get("minetogether.connect.title")));
         }
     }
