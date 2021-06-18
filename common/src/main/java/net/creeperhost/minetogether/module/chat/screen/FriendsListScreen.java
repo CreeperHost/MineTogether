7db3cca6a624e874ae799bc9137cc5efb39ab34d
Travis_N <travis_n@creeperhost.net>
2021-06-18 16:39:09 +0100

    Move ScrollingChat to its own class

--- common/src/main/java/net/creeperhost/minetogether/module/chat/screen/FriendsListScreen.java
+++ common/src/main/java/net/creeperhost/minetogether/module/chat/screen/FriendsListScreen.java
@@ -1,20 +1,10 @@
 package net.creeperhost.minetogether.module.chat.screen;
 
-import com.google.common.collect.Ordering;
-import com.mojang.blaze3d.platform.GlStateManager;
-import com.mojang.blaze3d.systems.RenderSystem;
-import com.mojang.blaze3d.vertex.BufferBuilder;
-import com.mojang.blaze3d.vertex.DefaultVertexFormat;
 import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.blaze3d.vertex.Tesselator;
-import net.creeperhost.minetogether.Constants;
 import net.creeperhost.minetogether.MineTogetherClient;
 import net.creeperhost.minetogether.handler.ToastHandler;
-import net.creeperhost.minetogether.module.chat.ChatFormatter;
-import net.creeperhost.minetogether.module.chat.ChatModule;
+import net.creeperhost.minetogether.module.chat.ScrollingChat;
 import net.creeperhost.minetogether.module.chat.screen.listentries.ListEntryFriend;
-import net.creeperhost.minetogether.module.multiplayer.data.PublicServerEntry;
-import net.creeperhost.minetogether.module.multiplayer.sort.ServerNameComparator;
 import net.creeperhost.minetogethergui.lists.ScreenList;
 import net.creeperhost.minetogethergui.widgets.ButtonMultiple;
 import net.creeperhost.minetogethergui.widgets.ButtonString;
@@ -22,25 +12,16 @@ import net.creeperhost.minetogetherlib.chat.ChatCallbacks;
 import net.creeperhost.minetogetherlib.chat.ChatHandler;
 import net.creeperhost.minetogetherlib.chat.KnownUsers;
 import net.creeperhost.minetogetherlib.chat.MineTogetherChat;
-import net.creeperhost.minetogetherlib.chat.data.Message;
 import net.creeperhost.minetogetherlib.chat.data.Profile;
-import net.creeperhost.minetogetherlib.util.LimitedSizeQueue;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.GuiComponent;
 import net.minecraft.client.gui.components.*;
 import net.minecraft.client.gui.screens.ConfirmScreen;
 import net.minecraft.client.gui.screens.Screen;
-import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.Style;
 import net.minecraft.network.chat.TranslatableComponent;
-import net.minecraft.util.FormattedCharSequence;
 import org.lwjgl.glfw.GLFW;
 
 import java.util.*;
 import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
-
-import static net.creeperhost.minetogetherlib.chat.ChatHandler.ircLock;
 
 public class FriendsListScreen extends Screen
 {
@@ -56,7 +37,6 @@ public class FriendsListScreen extends Screen
     {
         super(new TranslatableComponent("minetogether.friendscreen.title"));
         this.parent = parent;
-        String friendCode = ChatCallbacks.getFriendCode(MineTogetherClient.getUUID());
     }
 
     @Override
@@ -70,7 +50,7 @@ public class FriendsListScreen extends Screen
         {
             list.updateSize(100, height - 90, 28, this.height - 55);
         }
-        chat = new ScrollingChat(width - list.getRowWidth() - 22, this.height - 90, 32, this.height - 55);
+        chat = new ScrollingChat(this, width - list.getRowWidth() - 22, this.height - 90, 32, this.height - 55);
         chat.setLeftPos(list.getRowRight());
 
         chatBox = new EditBox(this.font, list.getRowRight() + 2, this.height -50, width - list.getRowWidth() - 7, 20, new TranslatableComponent(""));
@@ -151,6 +131,7 @@ public class FriendsListScreen extends Screen
     }
 
     public static ArrayList<Profile> removedFriends = new ArrayList<>();
+
     protected boolean refreshFriendsList()
     {
         List<Profile> friends = new ArrayList<Profile>();
@@ -298,229 +279,6 @@ public class FriendsListScreen extends Screen
                 res = str1.compareTo(str2);
             }
             return res;
-        }
-    }
-
-    public class ScrollingChat extends ObjectSelectionList
-    {
-        private ArrayList<FormattedCharSequence> lines;
-        private int height;
-        private int Width;
-        private int top;
-        private int bottom;
-        private int itemHeight;
-
-        public ScrollingChat(int width, int height)
-        {
-            super(Minecraft.getInstance(), width, height, 30, height, 10);
-            this.height = height;
-            this.width = width;
-            this.top = 30;
-            this.bottom = height;
-            this.itemHeight = 10;
-            lines = new ArrayList<>();
-            if(targetProfile != null) updateLines(targetProfile.getMediumHash());
-        }
-
-        public ScrollingChat(int widthIn, int heightIn, int topIn, int bottomIn)
-        {
-            super(Minecraft.getInstance(), widthIn, heightIn, topIn, bottomIn, 10);
-            this.height = height;
-            this.width = width;
-            this.top = 30;
-            this.bottom = height;
-            this.itemHeight = 10;
-            lines = new ArrayList<>();
-            if(targetProfile != null) updateLines(targetProfile.getMediumHash());
-        }
-
-        public void renderEntry(PoseStack poseStack, int index, int mouseX, int mouseY, float partialTicks)
-        {
-            try
-            {
-                FormattedCharSequence component = lines.get(index);
-                int totalWidth = 110;
-
-                int oldTotal = totalWidth;
-                totalWidth += minecraft.font.width(component);
-
-                boolean hovering = mouseX > oldTotal && mouseX < totalWidth && mouseY > getRowTop(index) && mouseY < getRowTop(index) + itemHeight;
-
-                Style style = minecraft.font.getSplitter().componentStyleAtWidth(component, (int) mouseX);
-
-                if(hovering)
-                {
-                    RenderSystem.enableBlend();
-                    RenderSystem.color4f(1, 1, 1, 0.90F);
-                    minecraft.font.draw(poseStack, component, oldTotal, getRowTop(index), 0xBBFFFFFF);
-                    renderComponentHoverEffect(poseStack, style , mouseX, mouseY);
-                    RenderSystem.color4f(1, 1, 1, 1);
-                }
-                else
-                {
-                    minecraft.font.draw(poseStack, component, oldTotal, getRowTop(index), 0xFFFFFF);
-                }
-            } catch (Exception ignored) {}
-        }
-
-        @Override
-        protected int getItemCount()
-        {
-            return lines.size();
-        }
-
-        protected void updateLines(String key)
-        {
-            LimitedSizeQueue<Message> tempMessages;
-            int oldMaxScroll = this.getMaxScroll();
-            synchronized (ircLock)
-            {
-                if (ChatHandler.messages == null || ChatHandler.messages.size() == 0) return;
-                tempMessages = ChatHandler.messages.get(key);
-            }
-
-            ArrayList<FormattedCharSequence> oldLines = lines;
-            int listHeight = this.height - (this.bottom - this.top - 4);
-            lines = new ArrayList<>();
-            if (tempMessages == null)
-                return;
-            try {
-                for (Message message : tempMessages) {
-                    Component display = ChatFormatter.formatLine(message);
-                    if (display == null)
-                        continue;
-                    lines.addAll(ComponentRenderUtils.wrapComponents(display, width - 10, font));
-                }
-            } catch (Exception ignored) {}
-            if (lines.size() > oldLines.size() && this.getScrollAmount() == oldMaxScroll) ;
-            {
-                this.setScrollAmount(this.getMaxScroll());
-            }
-        }
-
-        private int getRowBottom(int p_getRowBottom_1_)
-        {
-            return this.getRowTop(p_getRowBottom_1_) + this.itemHeight;
-        }
-
-        @Override
-        protected boolean isSelectedItem(int i)
-        {
-            return false;
-        }
-
-        @Override
-        protected int getScrollbarPosition()
-        {
-            return 0;
-        }
-
-        @Override
-        public void render(PoseStack matrixStack, int mouseX, int mouseY, float partialTicks)
-        {
-            this.renderBackground(matrixStack);
-            int i = this.getScrollbarPosition();
-            int j = i + 6;
-            Tesselator tessellator = Tesselator.getInstance();
-            BufferBuilder bufferbuilder = tessellator.getBuilder();
-            this.minecraft.getTextureManager().bind(GuiComponent.BACKGROUND_LOCATION);
-            RenderSystem.color4f(1.0F, 1.0F, 1.0F, 1.0F);
-            float f = 32.0F;
-            bufferbuilder.begin(7, DefaultVertexFormat.POSITION_TEX_COLOR);
-            bufferbuilder.vertex((double)this.x0, (double)this.y1, 0.0D).uv((float)this.x0 / 32.0F, (float)(this.y1 + (int)this.getScrollAmount()) / 32.0F).color(32, 32, 32, 255).endVertex();
-            bufferbuilder.vertex((double)this.x1, (double)this.y1, 0.0D).uv((float)this.x1 / 32.0F, (float)(this.y1 + (int)this.getScrollAmount()) / 32.0F).color(32, 32, 32, 255).endVertex();
-            bufferbuilder.vertex((double)this.x1, (double)this.y0, 0.0D).uv((float)this.x1 / 32.0F, (float)(this.y0 + (int)this.getScrollAmount()) / 32.0F).color(32, 32, 32, 255).endVertex();
-            bufferbuilder.vertex((double)this.x0, (double)this.y0, 0.0D).uv((float)this.x0 / 32.0F, (float)(this.y0 + (int)this.getScrollAmount()) / 32.0F).color(32, 32, 32, 255).endVertex();
-            tessellator.end();
-            int k = this.getRowLeft();
-            int l = this.y0 + 4 - (int)this.getScrollAmount();
-
-//            ScreenHelpers.drawLogo(matrixStack, font, width - 20, height + 18, 20, 30, 0.75F);
-            this.renderList(matrixStack, k, l, mouseX, mouseY, partialTicks);
-            this.minecraft.getTextureManager().bind(GuiComponent.BACKGROUND_LOCATION);
-            RenderSystem.enableDepthTest();
-            RenderSystem.depthFunc(519);
-            float f1 = 32.0F;
-            int i1 = -100;
-            bufferbuilder.begin(7, DefaultVertexFormat.POSITION_TEX_COLOR);
-            bufferbuilder.vertex((double)this.x0, (double)this.y0, -100.0D).uv(0.0F, (float)this.y0 / 32.0F).color(64, 64, 64, 255).endVertex();
-            bufferbuilder.vertex((double)(this.x0 + this.width), (double)this.y0, -100.0D).uv((float)this.width / 32.0F, (float)this.y0 / 32.0F).color(64, 64, 64, 255).endVertex();
-            bufferbuilder.vertex((double)(this.x0 + this.width), 0.0D, -100.0D).uv((float)this.width / 32.0F, 0.0F).color(64, 64, 64, 255).endVertex();
-            bufferbuilder.vertex((double)this.x0, 0.0D, -100.0D).uv(0.0F, 0.0F).color(64, 64, 64, 255).endVertex();
-            bufferbuilder.vertex((double)this.x0, (double)this.height, -100.0D).uv(0.0F, (float)this.height / 32.0F).color(64, 64, 64, 255).endVertex();
-            bufferbuilder.vertex((double)(this.x0 + this.width), (double)this.height, -100.0D).uv((float)this.width / 32.0F, (float)this.height / 32.0F).color(64, 64, 64, 255).endVertex();
-            bufferbuilder.vertex((double)(this.x0 + this.width), (double)this.y1, -100.0D).uv((float)this.width / 32.0F, (float)this.y1 / 32.0F).color(64, 64, 64, 255).endVertex();
-            bufferbuilder.vertex((double)this.x0, (double)this.y1, -100.0D).uv(0.0F, (float)this.y1 / 32.0F).color(64, 64, 64, 255).endVertex();
-            tessellator.end();
-            RenderSystem.depthFunc(515);
-            RenderSystem.disableDepthTest();
-            RenderSystem.enableBlend();
-            RenderSystem.blendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SourceFactor.ZERO, GlStateManager.DestFactor.ONE);
-            RenderSystem.disableAlphaTest();
-            RenderSystem.shadeModel(7425);
-            RenderSystem.disableTexture();
-            int j1 = 4;
-            bufferbuilder.begin(7, DefaultVertexFormat.POSITION_TEX_COLOR);
-            bufferbuilder.vertex((double)this.x0, (double)(this.y0 + 4), 0.0D).uv(0.0F, 1.0F).color(0, 0, 0, 0).endVertex();
-            bufferbuilder.vertex((double)this.x1, (double)(this.y0 + 4), 0.0D).uv(1.0F, 1.0F).color(0, 0, 0, 0).endVertex();
-            bufferbuilder.vertex((double)this.x1, (double)this.y0, 0.0D).uv(1.0F, 0.0F).color(0, 0, 0, 255).endVertex();
-            bufferbuilder.vertex((double)this.x0, (double)this.y0, 0.0D).uv(0.0F, 0.0F).color(0, 0, 0, 255).endVertex();
-            bufferbuilder.vertex((double)this.x0, (double)this.y1, 0.0D).uv(0.0F, 1.0F).color(0, 0, 0, 255).endVertex();
-            bufferbuilder.vertex((double)this.x1, (double)this.y1, 0.0D).uv(1.0F, 1.0F).color(0, 0, 0, 255).endVertex();
-            bufferbuilder.vertex((double)this.x1, (double)(this.y1 - 4), 0.0D).uv(1.0F, 0.0F).color(0, 0, 0, 0).endVertex();
-            bufferbuilder.vertex((double)this.x0, (double)(this.y1 - 4), 0.0D).uv(0.0F, 0.0F).color(0, 0, 0, 0).endVertex();
-            tessellator.end();
-
-            this.renderDecorations(matrixStack, mouseX, mouseY);
-            RenderSystem.enableTexture();
-            RenderSystem.shadeModel(7424);
-            RenderSystem.enableAlphaTest();
-            RenderSystem.disableBlend();
-        }
-
-        @Override
-        protected void renderList(PoseStack poseStack, int p_renderList_1_, int p_renderList_2_, int mouseX, int mouseY, float p_renderList_5_)
-        {
-            int i = lines.size();
-            Tesselator tessellator = Tesselator.getInstance();
-            BufferBuilder bufferbuilder = tessellator.getBuilder();
-
-            if(!lines.isEmpty())
-                for (int j = 0; j < i; ++j)
-                {
-                    int k = this.getRowTop(j);
-                    int l = this.getRowBottom(j);
-                    if (l >= this.y0 && k <= this.y1)
-                    {
-                        int i1 = p_renderList_2_ + j * this.itemHeight + this.headerHeight;
-                        int j1 = this.itemHeight - 4;
-                        int k1 = this.getRowWidth();
-                        //this.renderSelection &&
-                        if (this.isSelectedItem(j))
-                        {
-                            int l1 = this.x0 + this.width / 2 - k1 / 2;
-                            int i2 = this.x0 + this.width / 2 + k1 / 2;
-                            RenderSystem.disableTexture();
-                            float f = this.isFocused() ? 1.0F : 0.5F;
-                            RenderSystem.color4f(f, f, f, 1.0F);
-                            bufferbuilder.begin(7, DefaultVertexFormat.POSITION);
-                            bufferbuilder.vertex((double) l1, (double) (i1 + j1 + 2), 0.0D).endVertex();
-                            bufferbuilder.vertex((double) i2, (double) (i1 + j1 + 2), 0.0D).endVertex();
-                            bufferbuilder.vertex((double) i2, (double) (i1 - 2), 0.0D).endVertex();
-                            bufferbuilder.vertex((double) l1, (double) (i1 - 2), 0.0D).endVertex();
-                            tessellator.end();
-                            RenderSystem.color4f(0.0F, 0.0F, 0.0F, 1.0F);
-                            bufferbuilder.begin(7, DefaultVertexFormat.POSITION);
-                            bufferbuilder.vertex((double) (l1 + 1), (double) (i1 + j1 + 1), 0.0D).endVertex();
-                            bufferbuilder.vertex((double) (i2 - 1), (double) (i1 + j1 + 1), 0.0D).endVertex();
-                            bufferbuilder.vertex((double) (i2 - 1), (double) (i1 - 1), 0.0D).endVertex();
-                            bufferbuilder.vertex((double) (l1 + 1), (double) (i1 - 1), 0.0D).endVertex();
-                            tessellator.end();
-                            RenderSystem.enableTexture();
-                        }
-                        renderEntry(poseStack, j, mouseX, mouseY, p_renderList_5_);
-                    }
-                }
         }
     }
 }
