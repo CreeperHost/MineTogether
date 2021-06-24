66f8eda3648aab1ba721db68d05080a954b43904
Travis_N <travis_n@creeperhost.net>
2021-06-24 17:03:07 +0100

    Add missing import

--- common/src/main/java/net/creeperhost/minetogether/screen/SettingsScreen.java
+++ common/src/main/java/net/creeperhost/minetogether/screen/SettingsScreen.java
@@ -4,6 +4,7 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import net.creeperhost.minetogether.MineTogether;
 import net.creeperhost.minetogether.config.Config;
 import net.creeperhost.minetogether.module.chat.ChatModule;
+import net.creeperhost.minetogether.oauth.KeycloakOAuth;
 import net.creeperhost.minetogetherlib.chat.irc.IrcHandler;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.gui.components.Button;
