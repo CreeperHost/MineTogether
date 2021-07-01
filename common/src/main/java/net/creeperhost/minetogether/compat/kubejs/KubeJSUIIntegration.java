d5b829da502e91da75675eaa5bbec47944c315e1
Travis_N <travis_n@creeperhost.net>
2021-07-01 17:17:59 +0100

    Re-add cubejs support

---
+++ common/src/main/java/net/creeperhost/minetogether/compat/kubejs/KubeJSUIIntegration.java
@@ -0,0 +1,17 @@
+package net.creeperhost.minetogether.compat.kubejs;
+
+import net.creeperhost.minetogether.module.chat.screen.ChatScreen;
+import net.creeperhost.minetogether.module.chat.screen.FriendsListScreen;
+import net.creeperhost.minetogether.module.serverorder.screen.OrderServerScreen;
+import net.creeperhost.minetogetherlib.Order;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screens.Screen;
+
+import java.util.function.Consumer;
+
+public interface KubeJSUIIntegration
+{
+    Consumer<Screen> CHAT = screen -> Minecraft.getInstance().setScreen(new ChatScreen(screen));
+    Consumer<Screen> FRIENDS_LIST = screen -> Minecraft.getInstance().setScreen(new FriendsListScreen(screen));
+    Consumer<Screen> ORDER = screen -> Minecraft.getInstance().setScreen(OrderServerScreen.getByStep(0, new Order()));
+}
