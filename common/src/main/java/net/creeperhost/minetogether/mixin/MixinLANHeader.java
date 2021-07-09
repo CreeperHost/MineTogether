fa3e69b7b727bb740c0bd50aea3b80d47aebdfed
Travis_N <travis_n@creeperhost.net>
2021-07-09 16:29:02 +0100

    Fix Aarons mixin for lan scan

--- common/src/main/java/net/creeperhost/minetogether/mixin/MixinLANHeader.java
+++ common/src/main/java/net/creeperhost/minetogether/mixin/MixinLANHeader.java
@@ -5,20 +5,28 @@ import net.minecraft.client.gui.screens.multiplayer.ServerSelectionList;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TranslatableComponent;
 import org.objectweb.asm.Opcodes;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.gen.Invoker;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(ServerSelectionList.LANHeader.class)
+@Mixin(ServerSelectionList.class)
 public class MixinLANHeader
 {
     private static final Component OURSCANNING_LABEL = new TranslatableComponent("minetogether.connect.scan");
-    private static final Component SCANNING_LABEL = new TranslatableComponent("minetogether.connect.scan.offline");
+    private static final Component MTSCANNING_LABEL = new TranslatableComponent("minetogether.connect.scan.offline");
 
-    //TODO come back to this, No idea why this is breaking but it is...
-//    @Redirect(method = "render", at = @At(value = "FIELD", target = "Lnet/minecraft/client/gui/screens/multiplayer/ServerSelectionList;SCANNING_LABEL:Lnet/minecraft/network/chat/Component;", opcode = Opcodes.GETFIELD))
-//    private Component render(ServerSelectionList serverSelectionList)
-//    {
-//        return ConnectHelper.isEnabled ?  OURSCANNING_LABEL : SCANNING_LABEL;
-//    }
+    @Mutable
+    @Shadow @Final private static Component SCANNING_LABEL;
+
+    @Inject(at = @At("TAIL"), method = "<init>")
+    public void init(CallbackInfo ci)
+    {
+       SCANNING_LABEL = ConnectHelper.isEnabled ? OURSCANNING_LABEL : MTSCANNING_LABEL;
+    }
 }
