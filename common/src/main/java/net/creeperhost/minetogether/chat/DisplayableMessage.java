048899b6151b385d266bbd29f0484bcf56c478e7
covers1624 <laughlan.cov@internode.on.net>
2022-11-04 19:32:42 +1030

    Fix message change callbacks duplicating messages.
    ._.

--- common/src/main/java/net/creeperhost/minetogether/chat/DisplayableMessage.java
+++ common/src/main/java/net/creeperhost/minetogether/chat/DisplayableMessage.java
@@ -42,6 +42,7 @@ public abstract class DisplayableMessage<M> {
     public void format() {
         int addTime = builtMessage != null ? builtMessage.getAddedTime() : mc.gui.getGuiTicks();
         builtMessage = new GuiMessage<>(addTime, MessageFormatter.formatMessage(message), 0);
+        trimmedLines.clear();
 
         int maxLen = Mth.floor(getChatWidth());
         List<FormattedCharSequence> lines = ComponentRenderUtils.wrapComponents(builtMessage.getMessage(), maxLen, mc.font);
