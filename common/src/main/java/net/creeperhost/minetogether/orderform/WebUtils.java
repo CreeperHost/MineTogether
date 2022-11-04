1ae730a56331457e26177852389e7d9cd4a2198f
covers1624 <laughlan.cov@internode.on.net>
2022-11-04 16:19:13 +1030

    Move legacy web requests to new WebEngine.

--- common/src/main/java/net/creeperhost/minetogether/orderform/WebUtils.java
+++ common/src/main/java/net/creeperhost/minetogether/orderform/WebUtils.java
@@ -1,20 +1,20 @@
 package net.creeperhost.minetogether.orderform;
 
 import net.creeperhost.minetogether.MineTogether;
+import net.creeperhost.minetogether.lib.web.EngineRequest;
+import net.creeperhost.minetogether.lib.web.EngineResponse;
+import net.creeperhost.minetogether.lib.web.WebBody;
 import net.creeperhost.minetogether.util.ModPackInfo;
+import org.apache.commons.io.IOUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import javax.net.ssl.HttpsURLConnection;
-import java.io.*;
-import java.net.*;
-import java.nio.channels.Channels;
-import java.nio.channels.FileChannel;
-import java.nio.channels.ReadableByteChannel;
+import java.io.IOException;
+import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Path;
-import java.util.List;
 import java.util.Map;
+
+import static net.creeperhost.minetogether.MineTogether.WEB_ENGINE;
 
 // TODO, This needs to be replaced with the new API request system.
 @Deprecated
@@ -22,51 +22,22 @@ public class WebUtils {
 
     private static final Logger LOGGER = LogManager.getLogger();
     public static String userAgent = "";
-    private static List<String> cookies;
-    private static boolean logHide;
 
-    public static String getWebResponse(String urlString) throws IOException, URISyntaxException {
-        return getWebResponse(urlString, 0, false);
-    }
+    public static String getWebResponse(String urlString) throws IOException {
+        EngineRequest request = WEB_ENGINE.newRequest()
+                .method("GET", null)
+                .url(urlString)
+                .header("User-Agent", userAgent)
+                .header("Fingerprint", MineTogether.FINGERPRINT)
+                .header("Identifier", ModPackInfo.realName);
 
-    public static String getWebResponse(String urlString, int timeout, boolean print) throws IOException, URISyntaxException {
-        //If the request fails with domain ressolution and the domain is api.creeper.host try again using the IP address 84.54.54.84
-        if (timeout == 0) timeout = 120000;
-        URL url = new URL(urlString);
-        URI uri = new URI(url.getProtocol(), url.getUserInfo(), url.getHost(), url.getPort(), url.getPath(), url.getQuery(), url.getRef());
-        url = uri.toURL();
-        // lul
-        HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
-        conn.setInstanceFollowRedirects(true);
-        conn.setReadTimeout(timeout);
-
-        conn.setRequestMethod("GET");
-
-        if (cookies != null) {
-            for (String cookie : cookies) {
-                conn.addRequestProperty("Cookie", cookie.split(";", 2)[0]);
+        try (EngineResponse response = WEB_ENGINE.execute(request)) {
+            WebBody entity = response.body();
+            if (entity == null) {
+                return "";
             }
+            return IOUtils.toString(entity.open(), StandardCharsets.UTF_8);
         }
-        if (userAgent == null) userAgent = "";
-        conn.setRequestProperty("User-Agent", userAgent);
-        conn.setRequestProperty("Fingerprint", MineTogether.FINGERPRINT);
-        conn.setRequestProperty("Identifier", URLEncoder.encode(ModPackInfo.realName, "UTF-8"));
-        BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-        String line;
-        StringBuilder respData = new StringBuilder();
-        while ((line = rd.readLine()) != null) {
-            respData.append(line);
-            respData.append("\n");
-        }
-
-        List<String> setCookies = conn.getHeaderFields().get("Set-Cookie");
-
-        if (setCookies != null) {
-            cookies = setCookies;
-        }
-
-        rd.close();
-        return respData.toString();
     }
 
     private static String mapToFormString(Map<String, String> map) {
@@ -89,57 +60,28 @@ public class WebUtils {
         return postWebResponse(urlString, mapToFormString(postDataMap));
     }
 
-    public static String methodWebResponse(String urlString, String postDataString, String method, boolean isJson, boolean silent) throws IOException {
-        return methodWebResponse(urlString, postDataString, method, isJson ? "application/json" : "application/x-www-form-urlencoded", silent, 20000);
-    }
-
-    public static String methodWebResponse(String urlString, String postDataString, String method, String contentType, boolean silent, int timeout) throws IOException {
-        byte[] postData = postDataString.getBytes(StandardCharsets.UTF_8);
-        int postDataLength = postData.length;
-
-        URL url = new URL(urlString);
-
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setRequestProperty("User-Agent", userAgent);
-        conn.setRequestProperty("Fingerprint", MineTogether.FINGERPRINT);
-        conn.setRequestProperty("Identifier", URLEncoder.encode(ModPackInfo.realName, "UTF-8"));
-        conn.setRequestMethod(method);
-        if (cookies != null) {
-            for (String cookie : cookies) {
-                conn.addRequestProperty("Cookie", cookie.split(";", 2)[0]);
-            }
-        }
-        conn.setRequestProperty("Content-Type", contentType);
-        conn.setRequestProperty("charset", "utf-8");
-        conn.setRequestProperty("Content-Length", Integer.toString(postDataLength));
-        conn.setConnectTimeout(timeout);
-        conn.setReadTimeout(timeout);
-        conn.setUseCaches(false);
-        conn.setDoOutput(true);
-        conn.setInstanceFollowRedirects(true);
-        DataOutputStream wr = new DataOutputStream(conn.getOutputStream());
-        wr.write(postData);
-
-        BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-        String line;
-        StringBuilder respData = new StringBuilder();
-        while ((line = rd.readLine()) != null) {
-            respData.append(line);
-        }
-
-        List<String> setCookies = conn.getHeaderFields().get("Set-Cookie");
-
-        if (setCookies != null) {
-            cookies = setCookies;
-        }
-
-        rd.close();
-        logHide = false;
-        return respData.toString();
-    }
-
     public static String postWebResponse(String urlString, String postDataString) throws IOException {
-        return methodWebResponse(urlString, postDataString, "POST", false, false);
+        return methodWebResponse("POST", urlString, postDataString, false, false);
     }
 
+    public static String methodWebResponse(String method, String urlString, String postDataString, boolean isJson, boolean silent) throws IOException {
+        return methodWebResponse(method, urlString, postDataString, isJson ? "application/json" : "application/x-www-form-urlencoded", silent, 20000);
+    }
+
+    public static String methodWebResponse(String method, String urlString, String postDataString, String contentType, boolean silent, int timeout) throws IOException {
+        EngineRequest request = WEB_ENGINE.newRequest()
+                .method(method, WebBody.string(postDataString, contentType))
+                .url(urlString)
+                .header("User-Agent", userAgent)
+                .header("Fingerprint", MineTogether.FINGERPRINT)
+                .header("Identifier", ModPackInfo.realName)
+                .header("charset", "utf-8");
+        try (EngineResponse response = WEB_ENGINE.execute(request)) {
+            WebBody entity = response.body();
+            if (entity == null) {
+                return "";
+            }
+            return IOUtils.toString(entity.open(), StandardCharsets.UTF_8);
+        }
+    }
 }
