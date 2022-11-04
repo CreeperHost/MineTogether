a853cd96cca42d7444132e2a42499996e241da6b
covers1624 <laughlan.cov@internode.on.net>
2022-11-04 19:12:12 +1030

    Add mock framework for unit testing.

--- common/src/main/java/net/creeperhost/minetogether/lib/chat/ChatState.java
+++ common/src/main/java/net/creeperhost/minetogether/lib/chat/ChatState.java
@@ -4,6 +4,7 @@ import net.creeperhost.minetogether.lib.chat.irc.IrcClient;
 import net.creeperhost.minetogether.lib.chat.irc.pircbotx.PircBotClient;
 import net.creeperhost.minetogether.lib.chat.profile.ProfileManager;
 import net.creeperhost.minetogether.lib.web.ApiClient;
+import org.jetbrains.annotations.VisibleForTesting;
 
 /**
  * Created by covers1624 on 14/9/22.
@@ -17,13 +18,33 @@ public class ChatState {
     public final IrcClient ircClient;
     public final boolean logChatToConsole;
 
+    @VisibleForTesting
+    protected ChatState(ApiClient api, ChatAuth auth, MutedUserList mutedUserList, ProfileManager profileManager, IrcClient ircClient, boolean logChatToConsole) {
+        this.api = api;
+        this.auth = auth;
+        this.mutedUserList = mutedUserList;
+        this.profileManager = profileManager;
+        this.ircClient = ircClient;
+        this.logChatToConsole = logChatToConsole;
+    }
+
     public ChatState(ApiClient api, ChatAuth auth, MutedUserList mutedUserList, String realName, boolean logChatToConsole) {
         this.api = api;
         this.auth = auth;
         this.mutedUserList = mutedUserList;
-        profileManager = new ProfileManager(this, auth.getHash());
+        profileManager = new ProfileManager(auth.getHash()) {
+            @Override
+            public ChatState getChatState() {
+                return ChatState.this;
+            }
+        };
         profileManager.getOwnProfile().setPack(realName);
         ircClient = new PircBotClient(this, realName);
         this.logChatToConsole = logChatToConsole;
+
+        // Look these up ahead of time. May as well.
+        for (String mutedUser : mutedUserList.getMutedUsers()) {
+            profileManager.lookupProfileStale(mutedUser);
+        }
     }
 }
