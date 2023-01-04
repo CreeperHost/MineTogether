ed433fd6cfe993f8390a9c52bd45beb819109622
covers1624 <laughlan.cov@internode.on.net>
2023-01-04 20:50:50 +1030

    Improve compatibility with mods adjusting the pause menu.

--- common/src/main/java/net/creeperhost/minetogether/connect/MineTogetherConnect.java
+++ common/src/main/java/net/creeperhost/minetogether/connect/MineTogetherConnect.java
@@ -11,6 +11,9 @@ import net.minecraft.Util;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.components.AbstractWidget;
 import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.components.Widget;
+import net.minecraft.client.gui.components.events.GuiEventListener;
+import net.minecraft.client.gui.narration.NarratableEntry;
 import net.minecraft.client.gui.screens.ConfirmLinkScreen;
 import net.minecraft.client.gui.screens.PauseScreen;
 import net.minecraft.client.gui.screens.Screen;
@@ -18,6 +21,7 @@ import net.minecraft.client.server.IntegratedServer;
 import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.server.MinecraftServer;
 
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
@@ -42,40 +46,63 @@ public class MineTogetherConnect {
     }
 
     private static void onScreenOpen(Screen screen, ScreenAccess screenAccess) {
-        if (screen instanceof PauseScreen) {
-            IntegratedServer integratedServer = Minecraft.getInstance().getSingleplayerServer();
-            if (integratedServer != null) {
-                AbstractWidget feedBack = ButtonHelper.removeButton("menu.sendFeedback", screen);
-                AbstractWidget bugs = ButtonHelper.removeButton("menu.reportBugs", screen);
-                AbstractWidget openToLan = ButtonHelper.findButton("menu.shareToLan", screen);
-                AbstractWidget options = ButtonHelper.findButton("menu.options", screen);
+        if (!(screen instanceof PauseScreen)) return;
 
-                if (openToLan != null && feedBack != null) {
-                    openToLan.y = feedBack.y;
-                }
+        IntegratedServer integratedServer = Minecraft.getInstance().getSingleplayerServer();
+        if (integratedServer == null) return;
 
-                Button guiButton = new Button(screen.width / 2 - 102, screen.height / 4 + 72 + -16, 98, 20, new TranslatableComponent("minetogether.connect.open"), (button) -> Minecraft.getInstance().setScreen(new GuiShareToFriends(screen)));
+        @SuppressWarnings ("unchecked")
+        List<GuiEventListener> children = (List<GuiEventListener>) screen.children();
+        List<Widget> renderables = screenAccess.getRenderables();
+        List<NarratableEntry> narratables = screenAccess.getNarratables();
 
-                guiButton.active = !integratedServer.isPublished();
-                ScreenHooks.addRenderableWidget(screen, guiButton);
+        // Replace bugs button with our own button.
+        //TODO: move this to somewhere else, as not really relevant to the connect module
+        AbstractWidget bugs = ButtonHelper.findButton("menu.reportBugs", screen);
+        if (bugs != null && Config.instance().issueTrackerUrl != null) {
+            Button ourBugsButton = new Button(bugs.x, bugs.y, bugs.getWidth(), bugs.getHeight(), new TranslatableComponent("menu.reportBugs"), (button) -> {
+                String s = Config.instance().issueTrackerUrl;
+                Minecraft.getInstance().setScreen(new ConfirmLinkScreen((p_213069_2_) -> {
+                    if (p_213069_2_) {
+                        Util.getPlatform().openUri(s);
+                    }
 
-                if (bugs == null || feedBack == null) return;
-
-                //TODO: move this to somewhere else, as not really relevant to the connect module
-                Button ourFeedback = new Button(bugs.x, options.y, feedBack.getWidth(), 20, new TranslatableComponent("menu.reportBugs"), (button) ->
-                {
-                    String s = Config.instance().issueTrackerUrl;
-                    Minecraft.getInstance().setScreen(new ConfirmLinkScreen((p_213069_2_) ->
-                    {
-                        if (p_213069_2_) {
-                            Util.getPlatform().openUri(s);
-                        }
-
-                        Minecraft.getInstance().setScreen(screen);
-                    }, s, true));
-                });
-                ScreenHooks.addRenderableWidget(screen, ourFeedback);
-            }
+                    Minecraft.getInstance().setScreen(screen);
+                }, s, true));
+            });
+            // We have to keep these indexes the same and remove the old button due to how Mod Menu works...
+            children.set(children.indexOf(bugs), ourBugsButton);
+            renderables.set(renderables.indexOf(bugs), ourBugsButton);
+            narratables.set(narratables.indexOf(bugs), ourBugsButton);
+            bugs = ourBugsButton;
         }
+
+        AbstractWidget feedBack = ButtonHelper.findButton("menu.sendFeedback", screen);
+        AbstractWidget options = ButtonHelper.findButton("menu.options", screen);
+        if (!Config.instance().moveButtonsOnPauseMenu || feedBack == null || options == null) {
+            // Just add the button bellow the FriendsList button in the corner.
+            // We either didn't find the Feedback and Options buttons, or moving these buttons was disabled in our config.
+            Button guiButton = new Button(screen.width - 105, 25, 100, 20, new TranslatableComponent("minetogether.connect.open"), (button) -> Minecraft.getInstance().setScreen(new GuiShareToFriends(screen)));
+            ScreenHooks.addRenderableWidget(screen, guiButton);
+            return;
+        }
+
+        // Open To Friends button goes where the options button was.
+        Button guiButton = new Button(options.x, options.y, 98, 20, new TranslatableComponent("minetogether.connect.open"), (button) -> Minecraft.getInstance().setScreen(new GuiShareToFriends(screen)));
+
+        guiButton.active = !integratedServer.isPublished();
+        ScreenHooks.addRenderableWidget(screen, guiButton);
+
+        // Move the options button to where the feedback button was.
+        options.y = feedBack.y;
+        options.x = feedBack.x;
+
+        // Again, we have to juggle indexes because of Mod Menu...
+        children.remove(options);
+        renderables.remove(options);
+        narratables.remove(options);
+        children.set(children.indexOf(feedBack), options);
+        renderables.set(renderables.indexOf(feedBack), options);
+        narratables.set(narratables.indexOf(feedBack), options);
     }
 }
