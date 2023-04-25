b57ba6e784b1727199dc5bd82f9d877d5e19c2cc
covers1624 <laughlan.cov@internode.on.net>
2023-04-25 16:31:36 +0930

    Finish publishing impl.

--- common/src/main/java/net/creeperhost/minetogether/connect/ShareHandler.java
+++ common/src/main/java/net/creeperhost/minetogether/connect/ShareHandler.java
@@ -6,6 +6,7 @@ import net.creeperhost.minetogether.connect.netty.packet.SHostRegister;
 import net.creeperhost.minetogether.session.JWebToken;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.server.IntegratedServer;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.level.GameType;
 
 import java.util.concurrent.ExecutionException;
@@ -15,8 +16,8 @@ import java.util.concurrent.ExecutionException;
  */
 public class ShareHandler {
 
-    public static void publishToFriends(GameType gameMode, boolean commands) {
-        // TODO mostly copy of IntegratedServer#publishServer
+    public static void publishToFriends(GameType gameType, boolean cheats) {
+        // Mostly copy of IntegratedServer#publishServer
         Minecraft mc = Minecraft.getInstance();
         IntegratedServer server = mc.getSingleplayerServer();
         mc.prepareForMultiplayer();
@@ -28,5 +29,13 @@ public class ShareHandler {
         }
         NettyClient client = new NettyClient(server, "localhost", 32437, () -> new SHostRegister(token.toString()));
         client.start();
+        server.publishedPort = 0; // Doesn't matter, just set to _something_.
+        server.publishedGameType = gameType;
+        server.getPlayerList().setAllowCheatsForAllPlayers(cheats);
+        mc.player.setPermissionLevel(server.getProfilePermissions(mc.player.getGameProfile()));
+
+        for (ServerPlayer player : server.getPlayerList().getPlayers()) {
+            server.getCommands().sendCommands(player);
+        }
     }
 }
