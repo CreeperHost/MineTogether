973e219fba55b0f7134eb9b8bf67ab1f10cb0574
covers1624 <laughlan.cov@internode.on.net>
2023-05-03 14:33:19 +0930

    Yoink MineTogetherLib back into a library.

--- common/src/main/java/net/creeperhost/minetogether/lib/chat/profile/package-info.java
+++
@@ -1,4 +0,0 @@
-@NonNullApi
-package net.creeperhost.minetogether.lib.chat.profile;
-
-import net.creeperhost.minetogether.lib.annotation.NonNullApi;
--- common/src/main/java/net/creeperhost/minetogether/lib/web/apache/package-info.java
+++
@@ -1,4 +0,0 @@
-@NonNullApi
-package net.creeperhost.minetogether.lib.web.apache;
-
-import net.creeperhost.minetogether.lib.annotation.NonNullApi;
