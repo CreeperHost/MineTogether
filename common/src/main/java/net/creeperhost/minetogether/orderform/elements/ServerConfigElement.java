95eb439867be648a24356ea6422d22362972759d
brandon3055 <brandon4261@gmail.com>
2024-09-19 09:21:13 +1000

    Update order form

--- common/src/main/java/net/creeperhost/minetogether/orderform/elements/ServerConfigElement.java
+++ common/src/main/java/net/creeperhost/minetogether/orderform/elements/ServerConfigElement.java
@@ -22,10 +22,12 @@ import static net.creeperhost.polylib.client.modulargui.lib.geometry.GeoParam.*;
  */
 public class ServerConfigElement extends GuiElement<ServerConfigElement> {
 
+    private final OrderGui gui;
     private GuiTextField nameField;
 
     public ServerConfigElement(@NotNull GuiParent<?> parent, OrderGui gui) {
         super(parent);
+        this.gui = gui;
 
         Constraint midPos = relative(get(LEFT), 90);
 
@@ -78,24 +80,8 @@ public class ServerConfigElement extends GuiElement<ServerConfigElement> {
                 .setAlignment(Align.LEFT)
                 .constrain(TOP, relative(lastElement.get(BOTTOM), 5))
                 .constrain(LEFT, match(get(LEFT)))
-                .constrain(RIGHT, midPos)
+                .constrain(RIGHT, match(get(RIGHT)))
                 .constrain(HEIGHT, literal(14));
-
-        for (int i = 0; i < 5; i++) {
-            int finalI = i;
-            int count = 5 + (i * 5);
-            MTStyle.Flat.button(this, Component.literal(String.valueOf(count)))
-                    .setToggleMode(() -> gui.order.playerAmount == count)
-                    .onPress(() -> {
-                        gui.order.playerAmount = count;
-                        gui.locations.updateLocations();
-                        gui.summaryDirty();
-                    })
-                    .constrain(TOP, match(lastElement.get(TOP)))
-                    .constrain(BOTTOM, match(lastElement.get(BOTTOM)))
-                    .constrain(LEFT, dynamic(() -> midPos.get() + ((((xMax() + 1) - midPos.get()) / 5) * finalI)).precise())
-                    .constrain(WIDTH, dynamic(() -> (((xMax() + 1) - midPos.get()) / 5) - 1).precise());
-        }
 
         Component playerCountInfo = Component.translatable("minetogether:gui.order.player_count.info").withStyle(ChatFormatting.GRAY);
         lastElement = new GuiText(this, playerCountInfo)
@@ -103,10 +89,50 @@ public class ServerConfigElement extends GuiElement<ServerConfigElement> {
                 .setAlignment(Align.LEFT)
                 .constrain(TOP, relative(lastElement.get(BOTTOM), 2))
                 .constrain(LEFT, match(get(LEFT)))
-                .constrain(RIGHT, match(get(RIGHT)));
-        lastElement.constrain(HEIGHT, dynamic(() -> (double) this.font().wordWrapHeight(playerCountInfo, (int) xMax() - (int) xMin())));
+                .constrain(RIGHT, match(get(RIGHT)))
+                .autoHeight();
 
-        constrain(BOTTOM, match(lastElement.get(BOTTOM)));
+        GuiButton count5 = countButton(5)
+                .constrain(TOP, relative(lastElement.get(BOTTOM), 1))
+                .constrain(LEFT, match(get(LEFT)))
+                .constrain(WIDTH, dynamic(() -> (getValue(WIDTH) - 2D) / 2D))
+                .constrain(HEIGHT, literal(14));
+
+        GuiButton count10 = countButton(10)
+                .constrain(TOP, relative(lastElement.get(BOTTOM), 1))
+                .constrain(LEFT, relative(count5.get(RIGHT), 1))
+                .constrain(WIDTH, dynamic(() -> (getValue(WIDTH) - 2D) / 2D))
+                .constrain(HEIGHT, literal(14));
+
+        GuiButton count15 = countButton(15)
+                .constrain(TOP, relative(count10.get(BOTTOM), 1))
+                .constrain(LEFT, match(get(LEFT)))
+                .constrain(WIDTH, dynamic(() -> (getValue(WIDTH) - 2D) / 2D))
+                .constrain(HEIGHT, literal(14));
+
+        GuiButton count20 = countButton(20)
+                .constrain(TOP, relative(count10.get(BOTTOM), 1))
+                .constrain(LEFT, relative(count15.get(RIGHT), 1))
+                .constrain(WIDTH, dynamic(() -> (getValue(WIDTH) - 2D) / 2D))
+                .constrain(HEIGHT, literal(14));
+
+        GuiButton count25 = countButton(25)
+                .constrain(TOP, relative(count20.get(BOTTOM), 1))
+                .constrain(LEFT, midPoint(get(LEFT), get(RIGHT), () -> count20.xSize() / -2D))
+                .constrain(WIDTH, dynamic(() -> (getValue(WIDTH) - 2D) / 2D))
+                .constrain(HEIGHT, literal(14));
+
+        constrain(BOTTOM, match(count25.get(BOTTOM)));
 //        Constraints.bind(new GuiRectangle(this).border(0xFFFF0000), this);
+    }
+
+    private GuiButton countButton(int count) {
+        return MTStyle.Flat.button(this, Component.translatable("minetogether:gui.order.player_count." + count))
+                .setToggleMode(() -> gui.order.playerAmount == count)
+                .onPress(() -> {
+                    gui.order.playerAmount = count;
+                    gui.locations.updateLocations();
+                    gui.summaryDirty();
+                });
     }
 }
